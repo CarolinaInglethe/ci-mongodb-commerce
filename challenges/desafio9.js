7d3f326b3224760dd429c0ea26439dc98e30a269
Carol_Inglethe <carolinainglethe@gmail.com>
2021-11-23 17:20:02 -0300

    Requesito 9

---
+++ challenges/desafio9.js
@@ -0,0 +1,17 @@
+// Crie uma query que faça a remoção do último ingrediente no sanduíche Cheddar McMelt
+db.produtos.updateMany(
+    { nome: "Cheddar McMelt" },
+    {
+     $pop: {
+       ingredientes: 1,
+     },
+    },
+  );
+
+// Crie uma query que retorne o nome e ingredientes de todos os documentos.
+db.produtos.find(
+    {},
+    {
+      _id: false, nome: 1, ingredientes: 1,
+    },
+);
