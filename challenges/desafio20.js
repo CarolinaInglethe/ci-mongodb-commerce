b8effc0fa3c7e845a08d4619e0010460094a1a81
Carol_Inglethe <carolinainglethe@gmail.com>
2021-11-23 19:09:55 -0300

    Requesito 20

---
+++ challenges/desafio20.js
@@ -0,0 +1,11 @@
+// Crie uma query que faça a remoção do campo curtidas do item Big Mac.
+db.produtos.updateOne(
+    { nome: "Big Mac" },
+    { $unset: { curtidas: "" } },
+);
+
+// Crie uma query que retorne o nome e curtidas de todos os documentos.
+db.produtos.find(
+    {},
+    { _id: false, nome: 1, curtidas: 1 },
+);
