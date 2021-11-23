06ea3f46269dc835047aeb9897a6bde9b073da9e
Carol_Inglethe <carolinainglethe@gmail.com>
2021-11-23 17:45:26 -0300

    Requesito 10

---
+++ challenges/desafio10.js
@@ -0,0 +1,43 @@
+// Crie uma query que inclua um array com sete posições em todos os sanduíches. Cada uma delas representará um dia da semana, e cada posição iniciará em 0
+db.produtos.updateMany(
+   {},
+   {  
+     $push: {
+       vendasPorDia: {
+           $each: [0, 0, 0, 0, 0, 0, 0],
+       },
+     },  
+   },
+   { upsert: true },
+);
+
+// Crie uma query que incremente as vendas de Big Mac às quartas-feiras em 60.
+use("commerce");
+db.produtos.updateOne(
+  { nome: "Big Mac" },
+  {
+    $inc: { "vendasPorDia.3": 60 },
+  },
+);
+
+// Crie uma query que incremente as vendas de todos os sanduíches de carne do tipo bovino e pão aos sábados em 120.
+use("commerce");
+db.produtos.updateMany(
+  { 
+    tags: {
+      $all: ["bovino", "pão"],
+    }, 
+  },
+  {
+    $inc: { "vendasPorDia.6": 120 },
+  },
+);
+
+// Crie uma query que retorne o nome e vendasPorDia de todos os documentos.
+use("commerce");
+db.produtos.find(
+  {},
+  {
+    _id: false, nome: 1, vendasPorDia: 1,
+  },
+);
