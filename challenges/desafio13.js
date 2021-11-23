82e5e186d981844e11954da1c21a0dafb7125940
Carol_Inglethe <carolinainglethe@gmail.com>
2021-11-23 18:18:07 -0300

    Requesito 13

---
+++ challenges/desafio13.js
@@ -0,0 +1,28 @@
+// Crie uma query que faça a adição do elemento muito sódio ao final do array tags nos produtos em que o percentual de sódio seja maior ou igual a 40.
+db.produtos.updateMany(
+    { 
+      $and: [
+         {
+           "valoresNutricionais.tipo": "sódio", 
+         },
+        {  
+           "valoresNutricionais.percentual": { $gte: 40 }, 
+        },
+      ],
+      },
+    {  
+      $push: {
+        tags: "muito sódio",
+      },  
+    },
+    { upsert: true },
+ );
+
+//  Crie uma query que retorne o nome e tags de todos os documentos.
+use("commerce");
+db.produtos.find(
+  {},
+  {
+    _id: false, nome: 1, tags: 1,
+  },
+);
